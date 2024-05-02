def1668be359324689b8bc6be33052808b65bd78
franGhisoni <ghisonifran@gmail.com>
2024-05-02 15:56:57 -0300

    init ts

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config'
+import express from 'express'
+
+
+const app = express();
+app.use(express.json())
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`)
+})
+
+app.get('/ping',(_req,res) => {//_req es un parametro que no se usa y por typescript se marca con _
+    console.log(`llego la req!`)
+    res.send("pong")
+})
