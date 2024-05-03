406cf79d852d680aa4ce4956abf718b4f9916d5f
franGhisoni <ghisonifran@gmail.com>
2024-05-03 12:18:45 -0300

    sin serve

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import express from 'express'
 const app = express();
 app.use(express.json())
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 4000, () => {
     console.log(`Server running on port ${process.env.PORT}`)
 })
 
