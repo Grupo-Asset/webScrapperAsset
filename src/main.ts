4a85c3feb442bbeffdf66cc9afe7ad1dd82404bd
OtroAlien <flopysueiro1@gmail.com>
2024-05-08 17:16:39 -0300

    Código para pre-enrega

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import express from 'express'
 
 const app = express();
 app.use(express.json())
-
-app.listen(process.env.PORT || 4000, () => {
-    console.log(`Server running on port ${process.env.PORT}`)
+const port = process.env.PORT || 4000
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 })
 app.get('/', (_,res)=>{
     res.send(`
