0287ddbad97dd073a6d99de92b923c9e5fd01c4f
franGhisoni <ghisonifran@gmail.com>
2024-05-10 16:12:52 -0300

    titulo, precio y m2

--- src/Models/argenprop.ts
+++ src/Models/argenprop.ts
@@ -1,5 +1,5 @@
 import puppeteer from "puppeteer";
-
+// import { Terreno } from "./terreno";
 
 
 let scrap = async (req: {propertyType:string, transactionType:string})=> {
@@ -22,10 +22,35 @@ let scrap = async (req: {propertyType:string, transactionType:string})=> {
     })
     console.log("raw",rawElements)
     
-    
+    // const products :Array<Terreno> = []
     for(var i= 0 ; rawElements; i++){
         await page.goto(`https://www.argenprop.com${rawElements[i].link}`)
-        await page.goBack()    
+        const titleSelector = await page.waitForSelector('.titlebar__address');
+        const titulo = await titleSelector?.evaluate(el => el.textContent);
+        const precioSelector = await page.waitForSelector(".titlebar__price-mobile p")
+        const precio = await precioSelector?.evaluate(el => el.textContent?.replace(/[A-Z , \.]+/g,""))
+        const moneda = await precioSelector?.evaluate(el => el.textContent?.replace(/[0-9 , \.]+/g,""))
+        const m2Selector = await page.$('li[title="Superficie Total"] .strong')
+        const m2 = await m2Selector?.evaluate(el => el.textContent?.replace(/\D/g,''))
+
+        console.log("precio",precio? parseFloat(precio): 0,"Moneda",moneda,"titulo",titulo,"m2",m2?Number(m2):1)
+        await page.goBack()
+        // let moneda = document.querySelector("all")?.getAttribute("href")
+        // let m2 = document.querySelector("all")?.getAttribute("href")
+        // let ubicaicon = document.querySelector("all")?.getAttribute("href")
+        // let adicional =document.querySelector("all")?.getAttribute("href")
+        // let descripcion =document.querySelector("all")?.getAttribute("href")
+        // let alternativo = document.querySelector("all")?.getAttribute("href")
+        // let url =document.querySelector("all")?.getAttribute("href")
+        // let servicios =document.querySelector("all")?.getAttribute("href")
+        // let precioPorM2 = document.querySelector("all")?.getAttribute("href")
+        // let validacion = document.querySelector("all")?.getAttribute("href")
+        // let fechaDePublicacion = document.querySelector("all")?.getAttribute("href")
+        // let publicador = document.querySelector("all")?.getAttribute("href")
+        // let terreno = new Terreno()
+        // terreno.setTitulo(titulo? titulo : null)
+        // products.push(
+        // )
     }
     // await browser.close()
 }catch(error) {
---
+++ src/Models/terreno.ts
@@ -0,0 +1,164 @@
+class Terreno {
+    private titulo?              : string    | null = null;
+    private precio?              : number    | null = null;
+    private moneda?              : string    | null = null;
+    private m2?                  : number    | null = null;
+    private ubicacion?           : string    | null = null;
+    private adicional?           : string    | null = null;
+    private descripcion?         : string    | null = null;
+    private alternativo?         : string    | null = null;
+    private url?                 : string    | null = null;
+    private servicios?          : {
+        agua    : boolean   | null;
+        luz     : boolean   | null;
+        gas     : boolean   | null;
+        }                   | null                 = null;
+    private timeStamp?           : Date      | null = null;
+    private precioPorM2?         : number    | null = null;
+    private validacion?          : boolean   | null = null;
+    private fechaDePublicacion?  : Date      | null = null;
+    private publicador?          : string    | null = null;
+
+    // constructor(titulo: string, precio: number, moneda: string, m2: number, ubicacion: string, adicional: string, descripcion: string, alternativo: string, url: string, servicios: { agua: boolean; luz: boolean; gas: boolean; }, timeStamp: Date, precioPorM2: number, validacion: boolean, fechaDePublicacion: Date, publicador: string) {
+    //     this.titulo                 = titulo;
+    //     this.precio                 = precio;
+    //     this.moneda                 = moneda;
+    //     this.m2                     = m2;
+    //     this.ubicacion              = ubicacion;
+    //     this.adicional              = adicional;
+    //     this.descripcion            = descripcion;
+    //     this.alternativo            = alternativo;
+    //     this.url                    = url;
+    //     this.servicios              = servicios;
+    //     this.timeStamp              = new Date();
+    //     this.precioPorM2            = precioPorM2;
+    //     this.validacion             = validacion;
+    //     this.fechaDePublicacion     = fechaDePublicacion;
+    //     this.publicador             = publicador;
+    // }
+
+    constructor(){}
+    
+
+    getTitulo() {
+        return this.titulo;
+    }
+
+    getPrecio() {
+        return this.precio;
+    }
+
+    getMoneda() {
+        return this.moneda;
+    }
+
+    getM2() {
+        return this.m2;
+    }
+
+    getUbicacion() {
+        return this.ubicacion;
+    }
+
+    getAdicional() {
+        return this.adicional;
+    }
+
+    getDescripcion() {
+        return this.descripcion;
+    }
+
+    getAlternativo() {
+        return this.alternativo;
+    }
+
+    getUrl() {
+        return this.url;
+    }
+
+    getServicios() {
+        return this.servicios;
+    }
+
+    getTimeStamp() {
+        return this.timeStamp;
+    }
+
+    getPrecioPorM2() {
+        return this.precioPorM2;
+    }
+
+    getValidacion() {
+        return this.validacion;
+    }
+
+    getFechaDePublicacion() {
+        return this.fechaDePublicacion;
+    }
+
+    getPublicador() {
+        return this.publicador;
+    }
+
+    setTitulo(titulo: string | null) {
+        this.titulo = titulo;
+    }
+
+    setPrecio(precio: number | null) {
+        this.precio = precio;
+    }
+
+    setMoneda(moneda: string | null) {
+        this.moneda = moneda;
+    }
+
+    setM2(m2: number | null) {
+        this.m2 = m2;
+    }
+
+    setUbicacion(ubicacion: string | null) {
+        this.ubicacion = ubicacion;
+    }
+
+    setAdicional(adicional: string | null) {
+        this.adicional = adicional;
+    }
+
+    setDescripcion(descripcion: string | null) {
+        this.descripcion = descripcion;
+    }
+
+    setAlternativo(alternativo: string | null) {
+        this.alternativo = alternativo;
+    }
+
+    setUrl(url: string | null) {
+        this.url = url;
+    }
+
+    setServicios(servicios: { agua: boolean | null; luz: boolean | null; gas: boolean | null; }  | null) {
+        this.servicios = servicios;
+    }
+
+    setTimeStamp(timeStamp: Date) {
+        this.timeStamp = timeStamp;
+    }
+
+    setPrecioPorM2(precioPorM2: number | null) {
+        this.precioPorM2 = precioPorM2;
+    }
+
+    setValidacion(validacion:boolean | null) {
+        this.validacion = validacion;
+    }
+
+    setFechaDePublicacion(fechaDePublicacion: Date | null) {
+        this.fechaDePublicacion = fechaDePublicacion;
+    }
+
+    setPublicador(publicador: string | null) {
+        this.publicador = publicador;
+    }
+}
+
+export {Terreno}
