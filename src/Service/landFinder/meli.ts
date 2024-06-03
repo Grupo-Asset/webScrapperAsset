b32ebdf9c2889fb2a3221bb174712d09ef9f593d
OtroAlien <flopysueiro1@gmail.com>
2024-06-03 10:29:38 -0300

    Meli landfinder

--- src/Service/landFinder/meli.ts
+++ src/Service/landFinder/meli.ts
@@ -7,6 +7,13 @@ export const scrapMercadoLibre = async (): Promise<Terreno[]> => {
     const page = await browser.newPage();
     const terrenos: Terreno[] = [];
 
+    // const formatFechaArgentina = (fecha: Date): string => {
+    //     const dia = fecha.getDate().toString().padStart(2, '0');
+    //     const mes = (fecha.getMonth() + 1).toString().padStart(2, '0');
+    //     const anio = fecha.getFullYear();
+    //     return `${dia}/${mes}/${anio}`;
+    // };
+
     try {
         await page.goto(link);
         await page.waitForSelector('.ui-search-layout.ui-search-layout--grid');
@@ -33,10 +40,8 @@ export const scrapMercadoLibre = async (): Promise<Terreno[]> => {
 
             const moneda = await page.$eval('.andes-money-amount__currency-symbol', el => el.textContent?.trim() || '');
 
-
             const ubicacion = await page.$eval('.ui-pdp-media.ui-vip-location__subtitle', el => el.textContent?.trim() || '');
 
-            
             const descripcion = await page.$eval('.ui-pdp-description__content', el => el.textContent?.trim() || '');
 
             const m2 = await page.$$eval('.ui-pdp-highlighted-specs-res__icon-label', elements => {
@@ -50,50 +55,45 @@ export const scrapMercadoLibre = async (): Promise<Terreno[]> => {
                 return 0;
             });
 
-            const fechaDePublicacionTimestamp = await page.$eval('.ui-pdp-header .ui-pdp-header__bottom-subtitle', el => {
-                const text = el.textContent?.trim() || '';
-                const numberMatch = text.match(/\d+/);
-                const unidadMatch = text.match(/días|meses|año|años/);
+            // const selector = '.ui-pdp-seller-validated .ui-pdp-color--GRAY.ui-pdp-size--XSMALL.ui-pdp-family--REGULAR';
 
-                if (!numberMatch || !unidadMatch) {
-                    return new Date().getTime();
-                }
+            // const fechaDePublicacion = await page.$eval(selector, el => {
+            //     const text = el.textContent?.trim() || '';
+            //     const numberMatch = text.match(/\d+/);
+            //     const unidadMatch = text.match(/días|meses|año|años/);
 
-                const number = parseInt(numberMatch[0], 10);
-                const unidad = unidadMatch[0];
-                const fechaActual = new Date().getTime();
+            //     if (!numberMatch || !unidadMatch) {
+            //         return new Date().toISOString();
+            //     }
 
-                let fechaPublicacion;
-                switch (unidad) {
-                    case 'días':
-                        fechaPublicacion = fechaActual - (number * 24 * 60 * 60 * 1000);
-                        break;
-                    case 'meses':
-                        // Restar el número de meses a la fecha actual
-                        const fechaMeses = new Date();
-                        fechaMeses.setMonth(fechaMeses.getMonth() - number);
-                        fechaPublicacion = fechaMeses.getTime();
-                        break;
-                    case 'año':
-                    case 'años':
-                        // Restar el número de años a la fecha actual
-                        const fechaAños = new Date();
-                        fechaAños.setFullYear(fechaAños.getFullYear() - number);
-                        fechaPublicacion = fechaAños.getTime();
-                        break;
-                    default:
-                        // Si no se reconoce la unidad, devolver la fecha actual
-                        fechaPublicacion = fechaActual;
-                        break;
-                }
-            
-                return fechaPublicacion;
-            });
-            
-            // const fechaDePublicacion = new Date(fechaDePublicacionTimestamp);
-            
+            //     const number = parseInt(numberMatch[0]);
+            //     const unidad = unidadMatch[0];
+            //     const fechaActual = new Date();
 
-            // Establece todas las propiedades del terreno
+            //     switch (unidad) {
+            //         case 'días':
+            //             fechaActual.setDate(fechaActual.getDate() - number);
+            //             break;
+            //         case 'meses':
+            //             fechaActual.setMonth(fechaActual.getMonth() - number);
+            //             break;
+            //         case 'año':
+            //         case 'años':
+            //             fechaActual.setFullYear(fechaActual.getFullYear() - number);
+            //             break;
+            //         default:
+            //             console.warn('Unidad de tiempo desconocida:', unidad);
+            //             break;
+            //     }
+
+            //     return fechaActual.toISOString();
+            // });
+
+
+            // const fechaDePublicacionDate = new Date(fechaDePublicacion);
+
+            // const fechaDePublicacionFormatted = formatFechaArgentina(fechaDePublicacionDate);
+
             terreno.setTitulo(titulo);
             terreno.setPrecio(precio);
             terreno.setMoneda(moneda);
@@ -101,8 +101,7 @@ export const scrapMercadoLibre = async (): Promise<Terreno[]> => {
             terreno.setUbicacion(ubicacion);
             terreno.setDescripcion(descripcion);
             terreno.setUrl(element.link);
-            // terreno.setFechaDePublicacion(fechaDePublicacion);
-
+            terreno.setFechaDePublicacion(null);
             // Ajusta estas propiedades según sea necesario
             terreno.setPublicador('');
 
